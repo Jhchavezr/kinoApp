aab2a21ca62a0a000880b848ee8d87a9d2f13f49
jhchavezr <jhchavezr@gmail.com>
2025-02-25 11:00:01 -0600

    removed folder

--- testapps/mongodbtest.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-try:
-    client = MongoClient("mongodb://admin_user:Cic40MX@52.15.210.68:27017/admin")
-    client.admin.command("ping")  # Simple command to check the connection
-    print("Connection successful!")
-except Exception as e:
-    print("Failed to connect to MongoDB:", e)
